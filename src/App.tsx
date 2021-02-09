18a391e3453e92488ef7e3c6b765782b5c1811e7
Willian Toledo <williamtoll99@gmail.com>
2021-02-09 14:50:54 -0300

    Play Pause Skip buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Box } from "@material-ui/core";
+//@ts-ignore
+import Feed from "feed-media-audio-player";
 
 function App() {
+
+  useEffect(()=> {
+    var player = new Feed.Player("demo", "demo");
+  
+    // Display all the events the player triggers
+    player.on("all", function (event:any) {
+      console.log(
+        "player triggered event '" + event + "' with arguments:",
+        Array.prototype.splice.call(arguments, 1,1)
+      );
+    });
+    
+    var playerView = new Feed.PlayerView("player-view-div", player);
+    
+    player.tune();
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box component="span" m={1}>
+        <div id="player-view-div">
+          <div className='status'></div>
+          <Button className="play-button">Play</Button>
+          <Button className="pause-button">Pause</Button>
+          <Button className="skip-button">Skip</Button>
+        </div>
+      </Box>
     </div>
   );
 }
